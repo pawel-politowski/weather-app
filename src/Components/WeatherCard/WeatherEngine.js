861741bd4f8a890180b81ee56b1e4dc7e8e5de42
Paweł Politowski <matelly@poczta.onet.pl>
2020-04-29 16:21:11 +0200

    Almost done

--- src/Components/WeatherCard/WeatherEngine.js
+++ src/Components/WeatherCard/WeatherEngine.js
@@ -11,18 +11,27 @@ function WeatherEngine({location}) {
     condition: null,
     country: null
   });   
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const getWeather = async q => {
-    const response = await fetch(
+      setLoading(true);
+    try {
+        const response = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&appid=ebd06e595f8759f59e0433a50bf0ced1`
     );
     const resJSON = await response.json();
     setWeather({
       temp: resJSON.main.temp,
-      city: resJSON.name,
+      city: resJSON.name, 
       condition: resJSON.weather[0].main,
       country: resJSON.sys.country
       }); 
+    }
+    catch(error){
+        setError(true)
+    }
+      setLoading(false);      
   };
 
  
@@ -36,13 +45,18 @@ function WeatherEngine({location}) {
   }, [location])
 
   return (
-    <div>
-      <WeatherCard temp={weather.temp} condition={weather.condition} city={weather.city} country={weather.country}/>      
-      <form>
-        <input value={query} onChange={(event) => setQuery(event.target.value)}/>
-        <button onClick={event => handleSearch(event)}>Search</button>
-      </form>
-    </div>
+    <React.Fragment> 
+        { error ? <div style={{ color:"black"}}>Wpisz poprawną nazwę miasta lub spróbuj później!</div> : loading ? <div style={{ color:"black"}}>Trwa ładowanie danych</div> : 
+        <div>
+        <WeatherCard temp={weather.temp} condition={weather.condition} city={weather.city} country={weather.country}/>      
+        <form>
+            <input value={query} onChange={(event) => setQuery(event.target.value)}/>
+            <button onClick={event => handleSearch(event)}>Search</button>
+        </form>
+        </div>
+        }
+    </React.Fragment>
+      
   );
 }
 
